9342526e5483bbab2a54e8dffdc4f388f2fb17dc
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-12 04:06:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word (message)
   
-  arr_message = message.split()
+  arr_message = message.split(/ /)
   
-  arr_message.split(" ").each do |word|
+  arr_message.each do |word|
     word.reverse
   end
 end
